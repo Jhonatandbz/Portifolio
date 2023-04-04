002e807433dd386fff638a6bb1e9b1320de8e960
Jhonatan Oliveira <jhonatanoliveira315@gmail.com>
2023-04-04 17:09:07 -0300

    Feat: Create home screan

---
+++ pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function home(){
+    return (
+        <>
+            <div className="int">
+
+                <h1>Teste opora</h1>
+                
+            </div>
+        </>
+    )
+}
