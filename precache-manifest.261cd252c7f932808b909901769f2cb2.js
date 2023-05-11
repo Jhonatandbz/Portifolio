1033eddae804b13bb0debd90e700e8d11390b6ab
Jhonatan Oliveira <jhonatanoliveira315@gmail.com>
2023-05-11 10:56:04 -0300

    Updates

---
+++ precache-manifest.261cd252c7f932808b909901769f2cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c156cff7782a5431327",
+    "url": "/Portifolio/static/css/main.5bb2c995.chunk.css"
+  },
+  {
+    "revision": "1c156cff7782a5431327",
+    "url": "/Portifolio/static/js/main.1c156cff.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/Portifolio/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/Portifolio/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "55da7546723bd04719b6",
+    "url": "/Portifolio/static/js/runtime~main.55da7546.js"
+  },
+  {
+    "revision": "9d3058a6960b488a2be9787a4ff95702",
+    "url": "/Portifolio/index.html"
+  }
+];
